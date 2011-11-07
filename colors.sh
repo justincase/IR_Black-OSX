8f59abc369d11fdacdc050261cae11fec6018bc1
Justin Case <hello@justincase.cx>
2011-11-07 09:40:31 +0100

    Modified for portability between shells

--- colors.sh
+++ colors.sh
@@ -6,13 +6,16 @@
 # * Copy the script to your workstation, name it colors
 # * Make it executable: chmod +x colors
 # * Run it: ./colors
-# 
-echo -e "\e[0mCOLOR_NC (No color)"
-echo -e "\e[1;37mCOLOR_WHITE\t\e[0;30mCOLOR_BLACK"
-echo -e "\e[0;34mCOLOR_BLUE\t\e[1;34mCOLOR_LIGHT_BLUE"
-echo -e "\e[0;32mCOLOR_GREEN\t\e[1;32mCOLOR_LIGHT_GREEN"
-echo -e "\e[0;36mCOLOR_CYAN\t\e[1;36mCOLOR_LIGHT_CYAN"
-echo -e "\e[0;31mCOLOR_RED\t\e[1;31mCOLOR_LIGHT_RED"
-echo -e "\e[0;35mCOLOR_PURPLE\t\e[1;35mCOLOR_LIGHT_PURPLE"
-echo -e "\e[0;33mCOLOR_YELLOW\t\e[1;33mCOLOR_LIGHT_YELLOW"
-echo -e "\e[1;30mCOLOR_GRAY\t\e[0;37mCOLOR_LIGHT_GRAY"
+#
+# https://github.com/justincase/IR_Black-OSX/edit/master/colors.sh
+# Modified for portability between shells
+#
+printf "\033[0mCOLOR_NC (No color)\n"
+printf "\033[1;37mCOLOR_WHITE\t\033[0;30mCOLOR_BLACK\n"
+printf "\033[0;34mCOLOR_BLUE\t\033[1;34mCOLOR_LIGHT_BLUE\n"
+printf "\033[0;32mCOLOR_GREEN\t\033[1;32mCOLOR_LIGHT_GREEN\n"
+printf "\033[0;36mCOLOR_CYAN\t\033[1;36mCOLOR_LIGHT_CYAN\n"
+printf "\033[0;31mCOLOR_RED\t\033[1;31mCOLOR_LIGHT_RED\n"
+printf "\033[0;35mCOLOR_PURPLE\t\033[1;35mCOLOR_LIGHT_PURPLE\n"
+printf "\033[0;33mCOLOR_YELLOW\t\033[1;33mCOLOR_LIGHT_YELLOW\n"
+printf "\033[1;30mCOLOR_GRAY\t\033[0;37mCOLOR_LIGHT_GRAY\n"
